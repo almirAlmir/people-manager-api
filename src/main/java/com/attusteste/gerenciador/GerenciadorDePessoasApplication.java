3e34f105398dbbc8f7d3d8c50c87c9bc380d434f
almirAlmir <aasc@cin.ufpe.br>
2024-05-08 02:17:40 -0300

    Feat(people-manager): Spring project initializing

---
+++ src/main/java/com/attusteste/gerenciador/GerenciadorDePessoasApplication.java
@@ -0,0 +1,13 @@
+package com.attusteste.gerenciador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDePessoasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorDePessoasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/attusteste/gerenciador/GerenciadorDePessoasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attusteste.gerenciador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDePessoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
